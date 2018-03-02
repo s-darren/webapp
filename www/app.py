aeb5490cb053d1913a0e5c08f9ad8bed1f267a71
Darren <295744570@qq.com>
2018-03-02 14:43:43 +0800

    完成基本功能

--- www/app.py
+++ www/app.py
@@ -126,7 +126,7 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
-    await orm.create_pool(loop=loop, **configs['db'])
+    await orm.create_pool(loop=loop, **configs.db)
     app = web.Application(loop=loop, middlewares=[
         logger_factory, auth_factory ,response_factory
     ])
--- www/config_default.py
+++ www/config_default.py
@@ -5,15 +5,14 @@
 Default configurations.
 '''
 
-__author__ = 'Michael Liao'
 
 configs = {
     'debug': True,
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'root',
-        'password': '',
+        'user': 'www-data',
+        'password': 'www-data',
         'db': 'awesome'
     },
     'session': {
